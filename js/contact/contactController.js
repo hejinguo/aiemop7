ef5c9223720ce06bbd82ebcd9e47cd0c62700620
hejg <hejinguo727@163.com>
2017-12-25 15:58:55 +0800

    change-main-style

--- js/contact/contactController.js
+++ js/contact/contactController.js
@@ -1,7 +1,17 @@
 define(['app'],function(app){
+	var $$ = Dom7;
 	
 	function init(){
-		
+//		app.f7.showTab('#tab1');
+		/*
+		$$('#tab1').on('tab:show', function () {
+		    app.f7.alert('Tab 1 is visible');
+		});
+		 
+		$$('#tab2').on('tab:show', function () {
+		    app.f7.alert('Tab 2 is visible');
+		});
+		*/
 	}
 	
 	return {
