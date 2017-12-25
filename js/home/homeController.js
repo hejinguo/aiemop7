fe104dd7cdc79b9d497f9c8cd659164704df3ac1
hejg <hejinguo727@163.com>
2017-12-25 17:07:05 +0800

    change-style

--- js/home/homeController.js
+++ js/home/homeController.js
@@ -2,11 +2,25 @@ define(['app'],function(app) {
 	var $$ = Dom7;
 
 	function init() {
-//		var mySwiper = app.f7.swiper('.swiper-container', {
-//			pagination:'.swiper-pagination',
-//		    speed: 400,
-//		    spaceBetween: 100
-//		});   
+		function myGod(id,w,n){
+			var box=$$(id)[0],can=true,w=w||1500,fq=fq||10,n=n==-1?-1:1;
+			box.innerHTML+=box.innerHTML;
+			box.onmouseover=function(){can=false};
+			box.onmouseout=function(){can=true};
+			var max=parseInt(box.scrollHeight/2);
+			new function (){
+				var stop=box.scrollTop%18==0&&!can;
+				if(!stop){
+					var set=n>0?[max,0]:[0,max];
+					box.scrollTop==set[0]?box.scrollTop=set[1]:box.scrollTop+=n;
+//					console.log(box.scrollTop);
+				};
+				setTimeout(arguments.callee,box.scrollTop%18?fq:w);
+			};
+		};
+		
+		myGod('.theme-asiainfo .marquee-notice-list',2000);
+		
 		//mapView.render();
 	}
 	
