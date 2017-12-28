11c0598b8fa59b52ac8cbccc7d2b2e396095a2d3
hejg <hejinguo727@163.com>
2017-12-28 17:10:13 +0800

    change-notice-content

--- js/home/homeController.js
+++ js/home/homeController.js
@@ -4,13 +4,13 @@ define(['app','tool'],function(app,tool) {
 	function init() {
 		app.f7.addNotification({
 	        title: '系统公告',
-	        message: '用户您好,智慧政企移动办公管家目前已对您开放了‘商铺管理’、‘大数据报告’功能,其他的功能我们开发人员正在紧锣密鼓的赶制中,请知晓。'
+	        message: '用户您好,智慧政企移动办公管家目前已对您开放了‘商铺管理’、‘大数据报告’功能,其他的功能我们开发人员正在紧锣密鼓的赶制中,请关注后续发布的系统公告。'
 	    });
 		
 		var user = tool.getUser();
 		$$('.home-page .home-person-info .person-name').html(user.staffName+'('+user.staffCode+')');
 		$$('.home-page .home-person-info .person-subinfo span:first-child').html(user.organize.orgName+'_'+user.roles[0].roleName);
-		
+		$$('.home-page .home-person-info .person-subinfo span:last-child').html(user.phoneNo);
 		tool.marqueeNotice('.theme-asiainfo .marquee-notice-list',2000);
 	}
 	
